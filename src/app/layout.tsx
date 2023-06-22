501fe69ebd2d66bdbb63548e2141de402a162a39
Sreejit <sreejit9821@gmail.com>
2023-06-22 17:13:17 +0530

    Porfolio changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,18 +15,34 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={[inter.className, 'bg-image', 'text-gray-50','min-h-screen'].join(' ')}>
+      <body className={[inter.className, 'bg-gradient-to-b from-gray-900 to-gray-600 ', 'text-gray-50', 'min-h-screen'].join(' ')}>
         <header className='flex flex-col px-3 py-6 gap-2 md:mx-auto md:flex md:flex-row md:justify-between md:min-w-screen md:container '>
-            <div className='logo-section mx-auto md:mx-0'>
-              <span className='bg-gradient-to-br font-bold from-red-500 to-yellow-100 bg-clip-text text-3xl text-transparent'>Sreejit's blog</span>
-            </div>
-            <ul className='flex flex-row text-xl gap-5 mx-auto md:mx-0 md:text-2xl'>
-              <li>Blogs</li>
-              <li>Github</li>
-              <li>Twitter</li>
-            </ul>
+          <div className='logo-section mx-auto md:mx-0'>
+            <span className='bg-gradient-to-br font-bold from-red-500 to-yellow-100 bg-clip-text text-3xl text-transparent'>Sreejit's blog</span>
+          </div>
+          <ul className='flex flex-row text-xl gap-5 mx-auto md:mx-0 md:text-2xl'>
+            <li>Blogs</li>
+            <li>Github</li>
+            <li>Twitter</li>
+          </ul>
         </header>
         {children}
+        <footer className='mt-5 md:mt-14 md:container mx-auto'>
+          <div className='flex flex-col items-center justify-between gap-6 md:flex-row'>
+            <div className='blog-projects flex flex-col justify-center items-center md:item-start sm:w-7/12'>
+              <h2 className='text-3xl font-bold md:text-3xl md:font-bold 2xl:text-4xl 2xl:leading-10 2xl:font-bold'>Subscribe to my <span className='bg-gradient-to-br from-red-500 to-yellow-100 bg-clip-text text-transparent'>Newsletters</span> </h2>
+              <span className='text-lg text-gray-300 md:mb-7'>Subscribe to keep up to date</span>
+            </div>
+            <div className="w-90 mb-3 sm:w-5/12">
+              <form className="flex p-1 rounded-full bg-slate-800 px-4 py-2 focus-within:ring-2 focus-within:ring-cyan-600 hover:ring-2 hover:ring-cyan-600">
+                <input
+                  className="w-full appearance-none 
+              bg-slate-800 focus:outline-none"/>
+                <button className="ml-2 shrink-0 rounded-full bg-gradient-to-br from-sky-500 to-cyan-400 px-3 py-1 text-sm font-medium hover:from-sky-700 hover:to-cyan-600 focus:outline-none focus:ring-2 focus:ring-cyan-600/50" type="submit">Subscribe</button></form></div>
+          </div>
+
+          <div className="p-1 border-t border-white-600 pt-5 mb-10"><div className="text-sm text-gray-200">© Copyright 2023 Built with ♥ by Sreejit &  Snehal</div></div>
+        </footer>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,128 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className=''>
+      <section className='md:mt-5 md:container mx-auto'>
+        <div className='blog-summary'>
+          <h2 className='text-3xl font-bold md:text-3xl md:font-bold 2xl:text-4xl 2xl:leading-10 2xl:font-bold'>Recent <span className='bg-gradient-to-br from-red-500 to-yellow-100 bg-clip-text text-transparent'>Posts</span></h2>
+        </div>
+        <div className='mt-5 ring-1 hover:translate-y-1'>
+          <div className="flex flex-col items-center gap-x-8 rounded-md bg-[#00303d] p-3 md:flex-row md:bg-[#00303d]">
+            <div className="shrink-0"><a href="/demo/astro-boilerplate">
+              <img className="h-36 w-36 hover:translate-y-1 rounded-lg" src="/images/php.png" alt="Project Web Design" loading="lazy" />
+            </a></div>
+            <div>
+              <div className="flex flex-col items-center gap-y-2 md:flex-row">
+                <a className="hover:text-cyan-400" href="/demo/astro-boilerplate">
+                  <div className="text-xl font-semibold mt-1">PHP in 2023</div>
+                </a>
+                <div className="ml-3 flex gap-2">
+                  <div className="rounded-md px-2 py-1 text-xs font-semibold bg-fuchsia-400 text-fuchsia-900">Astro.js</div>
+                  <div className="rounded-md px-2 py-1 text-xs font-semibold bg-lime-400 text-lime-900">Web design</div>
+                  <div className="rounded-md px-2 py-1 text-xs font-semibold bg-sky-400 text-sky-900">Tailwind.css</div>
+                  <div className="rounded-md px-2 py-1 text-xs font-semibold bg-rose-400 text-rose-900">TypeScript</div>
+                </div></div><p className="mt-3 text-gray-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                  bibendum. Nunc non posuere consectetur, justo erat semper enim, non
+                  hendrerit dui odio id enim.</p>
+            </div>
+          </div>
+        </div>
+        <div className='mt-5 ring-1 hover:translate-y-1'>
+          <div className="flex flex-col items-center gap-x-8 rounded-md bg-[#00303d] p-3 md:flex-row">
+            <div className="shrink-0"><a href="/demo/astro-boilerplate">
+              <img className="h-36 w-36 hover:translate-y-1 rounded-lg" src="/images/php.png" alt="Project Web Design" loading="lazy" />
+            </a></div>
+            <div>
+              <div className="flex flex-col items-center gap-y-2 md:flex-row">
+                <a className="hover:text-cyan-400" href="/demo/astro-boilerplate">
+                  <div className="text-xl font-semibold mt-1">PHP in 2023</div>
+                </a>
+                <div className="ml-3 flex gap-2">
+                  <div className="rounded-md px-2 py-1 text-xs font-semibold bg-fuchsia-400 text-fuchsia-900">Astro.js</div>
+                  <div className="rounded-md px-2 py-1 text-xs font-semibold bg-lime-400 text-lime-900">Web design</div>
+                  <div className="rounded-md px-2 py-1 text-xs font-semibold bg-sky-400 text-sky-900">Tailwind.css</div>
+                  <div className="rounded-md px-2 py-1 text-xs font-semibold bg-rose-400 text-rose-900">TypeScript</div>
+                </div></div><p className="mt-3 text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                  bibendum. Nunc non posuere consectetur, justo erat semper enim, non
+                  hendrerit dui odio id enim.</p>
+            </div>
+          </div>
+        </div>
+        <div className='mt-5 ring-1 hover:translate-y-1'>
+          <div className="flex flex-col items-center gap-x-8 rounded-md bg-[#00303d] p-3 md:flex-row">
+            <div className="shrink-0"><a href="/demo/astro-boilerplate">
+              <img className="h-36 w-36 hover:translate-y-1 rounded-lg" src="/images/php.png" alt="Project Web Design" loading="lazy" />
+            </a></div>
+            <div>
+              <div className="flex flex-col items-center gap-y-2 md:flex-row">
+                <a className="hover:text-cyan-400" href="/demo/astro-boilerplate">
+                  <div className="text-xl font-semibold mt-1">PHP in 2023</div>
+                </a>
+                <div className="ml-3 flex gap-2">
+                  <div className="rounded-md px-2 py-1 text-xs font-semibold bg-fuchsia-400 text-fuchsia-900">Astro.js</div>
+                  <div className="rounded-md px-2 py-1 text-xs font-semibold bg-lime-400 text-lime-900">Web design</div>
+                  <div className="rounded-md px-2 py-1 text-xs font-semibold bg-sky-400 text-sky-900">Tailwind.css</div>
+                  <div className="rounded-md px-2 py-1 text-xs font-semibold bg-rose-400 text-rose-900">TypeScript</div>
+                </div></div><p className="mt-3 text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                  bibendum. Nunc non posuere consectetur, justo erat semper enim, non
+                  hendrerit dui odio id enim.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='mt-3 md:mt-14 md:container mx-auto'>
+        <div className='flex flex-row justify-between items-center'>
+          <div className='blog-projects'>
+            <h2 className='text-3xl font-bold md:text-3xl md:font-bold 2xl:text-4xl 2xl:leading-10 2xl:font-bold'>Recent <span className='bg-gradient-to-br from-red-500 to-yellow-100 bg-clip-text text-transparent'>Projects</span> </h2>
+          </div>
+          <div className='view-all-post'>
+            <span className='hover:text-blue-200 hover:cursor-pointer'>View all post →</span>
+          </div>
+        </div>
+
+        <div className='flex flex-col gap-5 md:flex md:flex-row md:mt-3 hover:cursor-pointer  md:justify-between'>
+          <div className="md:max-w-lg w-full hover:translate-y-2 ring-1 rounded-lg overflow-hidden shadow-lg bg-gradient-to-b from-[#006581] to-[#00303d]">
+            <img className="w-full" src="/images/php.png" alt="Sunset in the mountains" />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <p className="text-gray-100 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+              </p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+            </div>
+          </div>
+          <div className="md:max-w-lg w-full hover:translate-y-2 ring-1 rounded-lg overflow-hidden shadow-lg bg-gradient-to-b from-[#006581] to-[#00303d]">
+            <img className="w-full" src="/images/php.png" alt="Sunset in the mountains" />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <p className="text-gray-100 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+              </p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+            </div>
+          </div>
+          <div className="md:max-w-lg w-full hover:translate-y-2 ring-1 rounded-lg overflow-hidden shadow-lg bg-gradient-to-b from-[#006581] to-[#00303d]">
+            <img className="w-full" src="/images/php.png" alt="Sunset in the mountains" />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <p className="text-gray-100 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+              </p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+            </div>
+          </div>
+        </div>
 
       </section>
 
